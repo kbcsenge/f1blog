3dd9be740521005686285ccbee5234dacaab1724
Kovács-Bodó Csenge <h160431@stud.u-szeged.hu>
2024-04-30 23:34:07 +0200

    routing to driver's őage works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,26 @@ import {LoginComponent} from "./whenloggedout/login/login.component";
 import {RegisterComponent} from "./whenloggedout/register/register.component";
 import {LoginhomeComponent} from "./whenloggedin/loginhome/loginhome.component";
 import {NewpostComponent} from "./whenloggedin/newpost/newpost.component";
+import {MaxVerstappenComponent} from "./drivers/max-verstappen/max-verstappen.component";
+import {CharlesLeclercComponent} from "./drivers/charles-leclerc/charles-leclerc.component";
+import {CarlosSainzComponent} from "./drivers/carlos-sainz/carlos-sainz.component";
+import {LandoNorrisComponent} from "./drivers/lando-norris/lando-norris.component";
+import {LewisHamiltonComponent} from "./drivers/lewis-hamilton/lewis-hamilton.component";
+import {GeorgeRussellComponent} from "./drivers/george-russell/george-russell.component";
+import {SergioPerezComponent} from "./drivers/sergio-perez/sergio-perez.component";
+import {OscarPiastriComponent} from "./drivers/oscar-piastri/oscar-piastri.component";
+import {DanielRicciardoComponent} from "./drivers/daniel-ricciardo/daniel-ricciardo.component";
+import {PierreGaslyComponent} from "./drivers/pierre-gasly/pierre-gasly.component";
+import {EstebanOconComponent} from "./drivers/esteban-ocon/esteban-ocon.component";
+import {YukiTsunodaComponent} from "./drivers/yuki-tsunoda/yuki-tsunoda.component";
+import {KevinMagnussenComponent} from "./drivers/kevin-magnussen/kevin-magnussen.component";
+import {NicoHulkenbergComponent} from "./drivers/nico-hulkenberg/nico-hulkenberg.component";
+import {LoganSargeantComponent} from "./drivers/logan-sargeant/logan-sargeant.component";
+import {FernandoAlonsoComponent} from "./drivers/fernando-alonso/fernando-alonso.component";
+import {AlexanderAlbonComponent} from "./drivers/alexander-albon/alexander-albon.component";
+import {ValtteriBottasComponent} from "./drivers/valtteri-bottas/valtteri-bottas.component";
+import {GuanyuZhouComponent} from "./drivers/guanyu-zhou/guanyu-zhou.component";
+import {LanceStrollComponent} from "./drivers/lance-stroll/lance-stroll.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -14,8 +34,31 @@ export const routes: Routes = [
   { path : '' , component: HomeComponent}
 ];
 
+export const drivers: Routes =[
+  { path: 'verstappen', component: MaxVerstappenComponent },
+  { path: 'leclerc', component: CharlesLeclercComponent },
+  { path: 'sainz', component: CarlosSainzComponent },
+  { path : 'norris' , component: LandoNorrisComponent},
+  { path : 'hamilton' , component: LewisHamiltonComponent},
+  { path : 'russell' , component: GeorgeRussellComponent},
+  { path : 'perez' , component: SergioPerezComponent},
+  { path : 'piastri' , component: OscarPiastriComponent},
+  { path : 'ricciardo' , component: DanielRicciardoComponent},
+  { path : 'gasly' , component: PierreGaslyComponent},
+  { path : 'ocon' , component: EstebanOconComponent},
+  { path : 'tsunoda' , component: YukiTsunodaComponent},
+  { path : 'magnussen' , component: KevinMagnussenComponent},
+  { path : 'hulkenberg' , component: NicoHulkenbergComponent},
+  { path : 'sargeant' , component: LoganSargeantComponent},
+  { path : 'alonso' , component: FernandoAlonsoComponent},
+  { path : 'albon' , component: AlexanderAlbonComponent},
+  { path : 'bottas' , component: ValtteriBottasComponent},
+  { path : 'zhou' , component: GuanyuZhouComponent},
+  { path : 'stroll' , component: LanceStrollComponent}
+]
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), RouterModule.forRoot(drivers)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/drivers/drivers.component.ts
+++ src/app/drivers/drivers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-drivers',
@@ -6,5 +7,75 @@ import { Component } from '@angular/core';
   styleUrl: './drivers.component.scss'
 })
 export class DriversComponent {
+  constructor(private router: Router) {
+  }
 
+  verstappen(){
+    this.router.navigate(['/verstappen'])
+  }
+  leclerc(){
+    this.router.navigate(['/leclerc'])
+  }
+
+  sainz(){
+    this.router.navigate(['/sainz'])
+  }
+  norris(){
+    this.router.navigate(['/norris'])
+  }
+  hamilton(){
+    this.router.navigate(['/hamilton'])
+  }
+  russell(){
+    this.router.navigate(['/russell'])
+  }
+  perez(){
+    this.router.navigate(['/perez'])
+  }
+  piastri(){
+    this.router.navigate(['/piastri'])
+  }
+  ricciardo(){
+    this.router.navigate(['/ricciardo'])
+  }
+  gasly(){
+    this.router.navigate(['/gasly'])
+  }
+  ocon(){
+    this.router.navigate(['/ocon'])
+  }
+  tsunoda(){
+    this.router.navigate(['/tsunoda'])
+  }
+  magnussen(){
+    this.router.navigate(['/magnussen'])
+  }
+
+  hulkenberg(){
+    this.router.navigate(['/hulkenberg'])
+  }
+
+  sargeant(){
+    this.router.navigate(['/sargeant'])
+  }
+
+  alonso() {
+    this.router.navigate(['/alonso'])
+  }
+
+  albon(){
+    this.router.navigate(['/albon'])
+  }
+
+  bottas(){
+    this.router.navigate(['/bottas'])
+  }
+
+  zhou(){
+    this.router.navigate(['/zhou'])
+  }
+
+  stroll(){
+    this.router.navigate(['/stroll'])
+  }
 }
